ad1d8ec330bdd15fd1458a938ed57198c228391f
amytangzheng <amy.tang.zheng@gmail.com>
2025-04-20 16:48:08 -0400

    filter price

--- popup.js
+++ popup.js
@@ -95,8 +95,8 @@ async function fetchRestaurants() {
       // Foursquare API URL for searching restaurants
       // const url = 'https://api.foursquare.com/v3/places/search?radius=${milesToMeters(settings.distance)}&min_price=${settings.price[0]}&max_price=${settings.price[2]}';
 
-      const url = 'https://api.foursquare.com/v3/places/search?&min_price=${settings.price[0]}&max_price=${settings.price[2]}';
       console.log(settings.distance)
+      const url = 'https://api.foursquare.com/v3/places/search?&min_price=${settings.price[0]}&max_price=${settings.price[2]}';
 
       // Fetch restaurant data from Foursquare
       fetch(url, options)
